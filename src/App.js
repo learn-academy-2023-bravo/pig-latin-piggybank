a56bca8e142f38ac962f8975c36285579cb8fd30
wagnergon <wagnergon52@gmail.com>
2023-03-28 11:33:37 -0700

    vowel logic

--- src/App.js
+++ src/App.js
@@ -32,9 +32,9 @@ const App = () => {
       console.log("vowelsArray:", vowelsArray)
 
       // ACTION ITEM: your Pig Latin logic goes here!
-
-    
-
+    if (vowelsArray.includes(eachWord[0])){
+      return `${eachWord}way`
+    }
       // ACTION ITEM: this return will be the output of your Pig Latin'd code
       return eachWord
     })
